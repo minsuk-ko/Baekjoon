54022895f7e7704a589f3da91f74a5fcea0e2c0a
minsuk-ko <74853515+minsuk-ko@users.noreply.github.com>
2024-01-02 21:37:39 +0900

    Create baekjoon_1547.py

---
+++ baekjoon_1547.py
@@ -0,0 +1,20 @@
+N = int(input())
+score = list(map(int, input().split()))
+new_score = []
+max = score[0]
+for i in range(N):
+    if(max < score[i]):
+        max = score[i]
+for i in range(N):
+    if(max == score[i]):
+        new_score.append(100)
+    else:
+        new_score.append(score[i]/max * 100)
+
+sum =0
+for i in range(N):
+    sum += new_score[i]
+
+print(sum/N)
+        
+        
